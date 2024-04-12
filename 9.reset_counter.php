5fdf0c86e66044a8c8967e6504e350064565cd79
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:37:58 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    session_start();                    // 啟動session,要讀取/操作session變數之前，需呼叫session_start()，才能夠跨網頁
+    unset($_SESSION["counter"]);        // 刪除計數器的值,session_unset() 或者session_destroy()也可以達成同樣功能
+    echo "counter重置成功....";          // 輸出顯示"couter重置成功...."的訊息
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";        // 2秒過後跳入"8.counter.php"
 
 ?>
