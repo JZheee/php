1e74ae7e49e576e08f5cff0b6a0a6f3bb367dd6c
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 21:47:54 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -10,9 +10,9 @@
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");            // 使用mysqi_connect()函數建立與"db4free.net"資料庫的連結
         $result=mysqli_query($conn, "select * from bulletin");                              // 使用mysqli_query()函數向資料庫發送查詢bulletin資料表
         echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";        // 輸出表格開始標籤和標頭
-        while ($row=mysqli_fetch_array($result)){                                           
-            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a> 
-            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";
+        while ($row=mysqli_fetch_array($result)){                                            // 使用mysqli_fetch_array()函數從查詢結果中一行一行擷取資料,並將資料顯示到表格中
+            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a>       // 輸出表格開始標籤和表頭
+            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";             // 使用while迴圈從查詢結果中一行一行地擷取資料，並將資料顯示在表格中
             echo $row["bid"];
             echo "</td><td>";
             echo $row["type"];
