126709f9fbadf5a3e45f4d74e27c3ad6de96adf8
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 21:44:32 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -1,12 +1,12 @@
 <?php
     error_reporting(0);            // 關閉錯誤報告
-    session_start();
-    if (!$_SESSION["id"]) {
-        echo "請先登入";
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    session_start();               // 啟動session,要讀取/操作session變數之前，需呼叫session_start()，才能夠跨網頁
+    if (!$_SESSION["id"]) {        // 檢查用戶是否已登入
+        echo "請先登入";            // 如果未登入，顯示請求登入的訊息
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";        //3秒後跳入"2.login.html"
     }
-    else{
-        echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
+    else{                          
+        echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";        // 如果已經登入，顯示歡迎訊息和用戶名稱
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         $result=mysqli_query($conn, "select * from bulletin");
         echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
