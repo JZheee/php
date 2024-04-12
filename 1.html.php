c3c3a819b00647e2bc555086dad96ee4e78446bf
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 10:57:59 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,10 +1,10 @@
 <html>
     <head><title>html+php</title></head>      
     <body>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
-        <?php echo "Hello world!"; ?> 
+        <?php echo "Hello world!"; ?>         // 使用echo函數列印並顯示"Hello World!"字串 
+        <h1>html</h1>                         // 顯示html標題
+        <?php echo "Hello world!"; ?>         // 使用echo函數列印並顯示"Hello World!"字串 
+        <h1>html</h1>                         // 顯示html標題
+        <?php echo "Hello world!"; ?>         // 使用echo函數列印並顯示"Hello World!"字串 
     </body>
 </html>
