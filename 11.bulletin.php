19d7401495b66d8342bcf335414eb1d2ced182e2
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 21:51:00 +0800

    Update 11.bulletin.php

--- 11.bulletin.php
+++ 11.bulletin.php
@@ -11,8 +11,8 @@
         $result=mysqli_query($conn, "select * from bulletin");                              // 使用mysqli_query()函數向資料庫發送查詢bulletin資料表
         echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";        // 輸出表格開始標籤和標頭
         while ($row=mysqli_fetch_array($result)){                                            // 使用mysqli_fetch_array()函數從查詢結果中一行一行擷取資料,並將資料顯示到表格中
-            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a>       // 輸出表格開始標籤和表頭
-            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";             // 使用while迴圈從查詢結果中一行一行地擷取資料，並將資料顯示在表格中
+            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a>       // 輸出每一行佈告的資訊以及編輯和刪除的超連結
+            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";             
             echo $row["bid"];
             echo "</td><td>";
             echo $row["type"];
@@ -24,7 +24,7 @@
             echo $row["time"];
             echo "</td></tr>";
         }
-        echo "</table>";
+        echo "</table>";                    // 輸出表格結束標籤
     
     }
 
