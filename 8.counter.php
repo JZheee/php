9e8df52f69ec378d4bb56c13c4101287b6b3bc81
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:26:25 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["counter"]))
+    session_start();                            // 啟動session,要讀取/操作session變數之前，需呼叫session_start()，才能夠跨網頁
+    if (!isset($_SESSION["counter"]))           // 檢查是否設置計數器,假如沒有,則將計數器初始值為1
         $_SESSION["counter"]=1;
-    else
-        $_SESSION["counter"]++;
+    else                                            
+        $_SESSION["counter"]++;                 // 假如已有設置計數器,則將計數器值加1
 
-    echo "counter=".$_SESSION["counter"];
-    echo "<br><a href=9.reset_counter.php>重置counter</a>";
+    echo "counter=".$_SESSION["counter"];       // 輸出計數器的值
+    echo "<br><a href=9.reset_counter.php>重置counter</a>";        // 重置計數器
 ?>
