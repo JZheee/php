03287d15f3a73d00d7023338af7b3e8d29d17d01
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:51:15 +0800

    Update 4.mydb.php

--- 4.mydb.php
+++ 4.mydb.php
@@ -2,7 +2,7 @@
     #mysqli_connect() 建立資料庫連結
     $conn = mysqli_connect("db4free.net", "immust", "immustimmust", "immust");        // 使用mysqli_connect()函數建立與"db4free.net"資料庫連結
     #mysqli_query() 從資料庫查詢資料
-    $result=mysqli_query($conn, "select * from user");            // 使用mysqli_query()函數向資料庫發送查詢
+    $result=mysqli_query($conn, "select * from user");            // 使用mysqli_query()函數向資料庫發送查詢user資料表
     #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
     $row=mysqli_fetch_array($result);                             // 使用mysqli_fetch_array()函數從查詢結果中一行一行擷取資料
     echo $row["id"] . " " . $row["pwd"]."<br>";                   // 輸出第一行的資料
