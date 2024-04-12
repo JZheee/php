736e63562438f6a7d846b35f3afee971d3922a13
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:02:55 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,6 +1,6 @@
 <?php 
-    if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234"))
-        echo "登入成功";
+    if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234"))       // 利用if else 條件控制,判斷帳號和密碼是否符合要求
+        echo "登入成功";                                                // 如果"id" 和 "pwd" 的資料符合,顯示登入成功
     else
-        echo "登入失敗";
+        echo "登入失敗";                                                // 如果"id" 或 "pwd" 不符合,顯示登入失敗
 ?>
