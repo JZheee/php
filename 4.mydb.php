075b8b2c236dd256fe8e33e5aa00ba017bd17fae
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:12:20 +0800

    Update 4.mydb.php

--- 4.mydb.php
+++ 4.mydb.php
@@ -4,7 +4,7 @@
     #mysqli_query() 從資料庫查詢資料
     $result=mysqli_query($conn, "select * from user");            // 使用mysqli_query()函數向資料庫發送查詢
     #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-    $row=mysqli_fetch_array($result);                             // 使用myssqli_fetch_array()函數從查詢結果中一行一行擷取資料
+    $row=mysqli_fetch_array($result);                             // 使用mysqli_fetch_array()函數從查詢結果中一行一行擷取資料
     echo $row["id"] . " " . $row["pwd"]."<br>";                   // 輸出第一行的資料
     $row=mysqli_fetch_array($result);                             // 擷取下一行的資料
     echo $row["id"] . " " . $row["pwd"];                          // 輸出第二行的資料
