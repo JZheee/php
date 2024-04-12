6d5fc69957cab410db5a7232e55c6e77d14d685a
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:50:31 +0800

    Update 10.login.php

--- 10.login.php
+++ 10.login.php
@@ -2,7 +2,7 @@
    #mysqli_connect() 建立資料庫連結
    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");      // 使用mysqi_connect()函數建立與"db4free.net"資料庫的連結
    #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");
+   $result=mysqli_query($conn, "select * from user");         // 使用mysqli_query()函數向資料庫發送查詢user資料表
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
    $login=FALSE;
    while ($row=mysqli_fetch_array($result)) {
