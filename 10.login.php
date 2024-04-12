be72021513fbf92336ade180b999e026b02e8128
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:53:19 +0800

    Update 10.login.php

--- 10.login.php
+++ 10.login.php
@@ -5,7 +5,7 @@
    $result=mysqli_query($conn, "select * from user");         // 使用mysqli_query()函數向資料庫發送查詢user資料表
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
    $login=FALSE;                                              // 設定初始登入狀態為FALSE
-   while ($row=mysqli_fetch_array($result)) {
+   while ($row=mysqli_fetch_array($result)) {                 // 使用while迴圈從查詢結果中一筆一筆的擷取資料
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
        $login=TRUE;
      }
