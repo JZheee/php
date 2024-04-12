29bf0caeea774f4c5ecdee46da2713c2af1de2b9
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:51:28 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -2,7 +2,7 @@
    #mysqli_connect() 建立資料庫連結
    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");      // 使用mysqli_connect()函數建立與資料的連結
    #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");         // 使用mysqli_query()函數向資料庫發送查詢
+   $result=mysqli_query($conn, "select * from user");         // 使用mysqli_query()函數向資料庫發送查詢user資料表
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
    while ($row=mysqli_fetch_array($result)) {                 // 使用While迴圈從查詢結果一筆一筆的擷取資料
      echo $row["id"]." ".$row["pwd"]."<br>";                  // 輸出每一行的 "id" 和 "pwd" 的字段
