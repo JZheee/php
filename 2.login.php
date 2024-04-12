d343ff04699b26759b0e8b0fd94e9d467c39c87e
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:00:29 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"];
-    echo "<br>";
-    echo $_POST["pwd"];
+    echo $_POST["id"];        //輸出顯示從"2.login.php"表單中輸入的帳號
+    echo "<br>";              // 插入換行,<br>是HTML標記,用於在網頁上插入一個斷行(換行)
+    echo $_POST["pwd"];       //輸出顯示從"2.login.php"表單中輸入的密碼
 ?>
