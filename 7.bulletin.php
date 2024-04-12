15086aa4a5e3c3e8cbe394683e9b1425f2ece462
JZheee <142671049+JZheee@users.noreply.github.com>
2024-04-12 11:11:59 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -1,9 +1,9 @@
 <?php
     error_reporting(0);
-    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-    $result=mysqli_query($conn, "select * from bulletin");
-    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
-    while ($row=mysqli_fetch_array($result)){
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");            // 使用mysqi_connect()函數建立與"db4free.net"資料庫的連結
+    $result=mysqli_query($conn, "select * from bulletin");                              // 使用mysqli_query()函數向資料庫發送查詢
+    echo "<table border=2><tr><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";        // 輸出表格開始標籤和標頭
+    while ($row=mysqli_fetch_array($result)){            // 使用mysqli_fetch_array()函數從查詢結果中一行一行擷取資料,並將資料顯示到表格中
         echo "<tr><td>";
         echo $row["bid"];
         echo "</td><td>";
@@ -16,5 +16,5 @@
         echo $row["time"];
         echo "</td></tr>";
     }
-    echo "</table>"
+    echo "</table>"                    // 輸出表個結束標籤
 ?>
