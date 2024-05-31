5c586edf111af6fe08b12a49f2b4514819bc02ca
JZheee <142671049+JZheee@users.noreply.github.com>
2024-05-31 09:50:26 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -2,6 +2,6 @@
     session_start();            // 啟動session,要讀取/操作session變數之前，需呼叫session_start()，才能夠跨網頁
     unset($_SESSION["id"]);     // 移除session中的帳號
     echo "登出成功....";         // 顯示登出成功的訊息
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";    // 設定網頁在3秒之後移至"2.login.html"網頁
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";    // 設定網頁在3秒之後跳入"2.login.html"網頁
 
 ?>
